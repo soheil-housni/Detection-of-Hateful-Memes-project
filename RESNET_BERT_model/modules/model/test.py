d4d88f2436ae33b806cec9392744138bd16f1ba5
Soheil Housni <soheil.housni@gmail.com>
2026-05-01 12:13:14 -0400

    End of the testing of the resnet distilbert model

--- RESNET_BERT_model/modules/model/test.py
+++ RESNET_BERT_model/modules/model/test.py
@@ -34,7 +34,8 @@ class Test():
         self.criterion=loss_fn
 
     def run_testing(self,
-                    test_dataloader:DataLoader):
+                    test_dataloader:DataLoader,
+                    path:str):
         
         logger.info("Testing run:")
         self.model.eval()
@@ -42,6 +43,7 @@ class Test():
             all_predictions=[]
             all_targets=[]
             batch_losses=[]
+            all_logits=[]
 
             for batch in test_dataloader:
                 batch["images"] = batch["images"].float().to(self.device)
@@ -70,9 +72,13 @@ class Test():
 
                 all_predictions.append(predictions)
                 all_targets.append(targets)
+
+                all_logits.append(logits)
             
             all_predictions=torch.cat(all_predictions)
             all_targets=torch.cat(all_targets)
+            all_logits=torch.cat(all_logits)
+            log_probs=torch.nn.functional.softmax(all_logits,dim=1)
 
             f1=f1_score(all_targets.cpu().numpy(),all_predictions.cpu().numpy(),average="weighted")
             accuracy=accuracy_score(all_targets.cpu().numpy(),all_predictions.cpu().numpy())
@@ -82,7 +88,21 @@ class Test():
             logger.info(f"Test F1 Score = {f1}")
             logger.info(f"Test Accuracy: = {accuracy}")
 
-            return f1,accuracy,final_loss,all_predictions.numpy(),all_targets.numpy(),logits.numpy()
+            all_predictions=all_predictions.numpy()
+            all_targets=all_targets.numpy()
+            log_probs=log_probs.numpy()
+
+            performances={
+                "test_f1":f1,
+                "accuracy":accuracy,
+                "loss":loss,
+                "predictions":all_predictions,
+                "targets":all_targets,
+                "log_probs":log_probs
+            }
+            torch.save(performances,f"{path}/test_performances.pt")
+
+            return f1,accuracy,final_loss,all_predictions,all_targets,log_probs
 
 
 
